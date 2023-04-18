fe358698e11038fb83d36ce963a21fb04d2bafa4
Nathan Cho <ntcho13@gmail.com>
2023-04-18 19:24:00 -0400

    Update parser.py

--- backend/parser.py
+++ backend/parser.py
@@ -1,26 +1,38 @@
-# Import Module
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
+import re
 
-# Website URL
-URL = 'https://inside.bard.edu/academic/courses/current/film.html'
+url = 'https://inside.bard.edu/academic/courses/current/film.html'
+# url = 'https://inside.bard.edu/academic/courses/spring2019/photography.html'
+# url = 'https://inside.bard.edu/academic/courses/fall2013/mathematics.html'
+# url = 'https://inside.bard.edu/academic/courses/spring2010/classic.html'
+# url = 'https://inside.bard.edu/academic/courses/fall2001/economics.html'
+# url = 'https://inside.bard.edu/academic/courses/fall96/paint.htm'
+response = requests.get(url)
 
-# Page content from Website URL
-page = requests.get(URL)
+soup = BeautifulSoup(response.content, 'html.parser')
 
-# Function to remove tags
-def remove_tags(html):
+text = soup.get_text()
 
-	# parse html content
-	soup = BeautifulSoup(html, "html.parser")
+scrapes = {
+	"crn": re.findall(r'\d{5}', text),
+	"credits": re.findall(r'redits[\S\s]*?(\d)', text),
+	"course": re.findall(r'[A-Z\/]{2,9}\s[0-9]{3,4}[ \S]*', text),
+	"professor": re.findall(r'ofessor[\S\s]*?(\w.+)', text),
+}
 
-	for data in soup(['style', 'script']):
-		# Remove tags
-		data.decompose()
-
-	# return data by retrieving the tag content
-	return ' '.join(soup.stripped_strings)
+with open('scrape.txt', 'w') as f:
+    f.write(text)
 
 
-# Print the extracted data
-print(remove_tags(page.content))
+# print(scrapes)
+
+print("crn\tcourse\tcredits\tprofessor")
+
+for i in range(len(scrapes["crn"])):
+	print("{0}\t{1}\t{2}\t{3}".format(
+		scrapes["crn"][i],
+		scrapes["course"][i],
+		scrapes["credits"][i],
+		scrapes["professor"][i],
+	))
